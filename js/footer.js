8a3d2811fe3046c291aa2cd5d33bebe55b1aba85
cyklon73 <cyklon698@gmail.com>
2022-07-29 16:35:22 +0200

    updated

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+let youtubeSelectToggeld = false;
+
+function toggleFooterYoutubeSelect() {
+    if (youtubeSelectToggeld) {
+        youtubeSelectToggeld = false;
+        document.getElementById("footer-youtube-selection").style.visibility = "hidden";
+    } else {
+        youtubeSelectToggeld = true;
+        document.getElementById("footer-youtube-selection").style.visibility = "visible";
+    }
+}
